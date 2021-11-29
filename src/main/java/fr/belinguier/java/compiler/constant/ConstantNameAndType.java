c47092ef45df36392da303f929f997e0e2b000a9
Eliott Belinguier <eliott@belinguier.fr>
2021-11-29 11:06:16 +0100

    Add NameAndType Constant with units test.

---
+++ src/main/java/fr/belinguier/java/compiler/constant/ConstantNameAndType.java
@@ -0,0 +1,96 @@
+package fr.belinguier.java.compiler.constant;
+
+import java.nio.ByteBuffer;
+import java.util.Objects;
+
+/**
+ * This Constant class represents name and type constant in the structure of the ClassFile.
+ * @author Eliott Belinguier
+ * @since 1.0
+ * @version 1.0
+ * @see Constant
+ * @see <a href="https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.4.6">ConstantNameAndType in ClassFile's structure</a>
+ */
+public class ConstantNameAndType extends Constant {
+
+    /**
+     * This field contains a constant that represents the name of a {@link java.lang.reflect.Field} or a {@link java.lang.reflect.Method}.
+     * @since 1.0
+     * @see ConstantUtf8
+     */
+    private final ConstantUtf8 name;
+
+    /**
+     * This field contains a constant that represents the description of a {@link java.lang.reflect.Field} or a {@link java.lang.reflect.Method}.
+     * @since 1.0
+     * @see ConstantUtf8
+     */
+    private final ConstantUtf8 descriptor;
+
+    /**
+     * Initializes a newly created Constant object to represent the name and description of a {@link java.lang.reflect.Field} or a {@link java.lang.reflect.Method}.
+     * @param name Name of the field or the method to be represented, as a {@link ConstantUtf8}.
+     * @param descriptor Description of the field or the method to be as a {@link ConstantUtf8}.
+     * @since 1.0
+     */
+    public ConstantNameAndType(ConstantUtf8 name, ConstantUtf8 descriptor) {
+        super(ConstantType.NAME_AND_TYPE);
+        this.name = name;
+        this.descriptor = descriptor;
+    }
+
+    /**
+     * Initializes a newly created Constant object to represent the name and description of a {@link java.lang.reflect.Field} or a {@link java.lang.reflect.Method}.
+     * @param name Name of the {@link java.lang.reflect.Field} or the {@link java.lang.reflect.Method} to be represented.
+     * @param descriptor Description of the {@link java.lang.reflect.Field} or the {@link java.lang.reflect.Method} to be.
+     * @since 1.0
+     */
+    public ConstantNameAndType(String name, String descriptor) {
+        this(new ConstantUtf8(name), new ConstantUtf8(descriptor));
+    }
+
+    /**
+     * Get the name of the {@link java.lang.reflect.Field} or {@link java.lang.reflect.Method} represented.
+     * @return The name of the {@link java.lang.reflect.Field} or {@link java.lang.reflect.Method} as a constant.
+     * @since 1.0
+     */
+    public ConstantUtf8 getName() {
+        return this.name;
+    }
+
+    /**
+     * Get the description of the {@link java.lang.reflect.Field} or {@link java.lang.reflect.Method} represented.
+     * @return The description of the {@link java.lang.reflect.Field} or {@link java.lang.reflect.Method} as a constant.
+     * @since 1.0
+     */
+    public ConstantUtf8 getDescriptor() {
+        return this.descriptor;
+    }
+
+    @Override
+    public int serializationSize() {
+        return super.serializationSize() + 4;
+    }
+
+    @Override
+    public byte[] serialize(ConstantPool constantPool) {
+        final ByteBuffer byteBuffer;
+        final short nameIndex;
+        final short descriptorIndex;
+
+        if (constantPool == null)
+            return null;
+        byteBuffer = ByteBuffer.allocate(serializationSize());
+        nameIndex = constantPool.getOrRegister(this.name);
+        descriptorIndex = constantPool.getOrRegister(this.descriptor);
+        byteBuffer.put((byte) getConstantType().getTag());
+        byteBuffer.putShort(nameIndex);
+        byteBuffer.putShort(descriptorIndex);
+        return byteBuffer.array();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getConstantType(), this.name, this.descriptor);
+    }
+}
---
+++ src/test/java/fr/belinguier/java/compiler/constant/TestConstantNameAndType.java
@@ -0,0 +1,70 @@
+package fr.belinguier.java.compiler.constant;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestConstantNameAndType {
+
+    @Test
+    public void testConstant() {
+        final ConstantPool constantPool = new ConstantPool();
+        final ConstantNameAndType constant = new ConstantNameAndType(new ConstantUtf8("Name"),
+                new ConstantUtf8("Descriptor"));
+        final byte[] serializedConstant = new byte[] {
+                ConstantType.NAME_AND_TYPE.getTag(), 0, 1, 0, 2
+        };
+
+        assertEquals(constant.getConstantType(), ConstantType.NAME_AND_TYPE);
+        assertEquals(constant.getName(), new ConstantUtf8("Name"));
+        assertEquals(constant.getDescriptor(), new ConstantUtf8("Descriptor"));
+        assertEquals(constant.serializationSize(), 5);
+        assertArrayEquals(constant.serialize(constantPool), serializedConstant);
+    }
+
+    @Test
+    public void testConstantNullName() {
+        final ConstantPool constantPool = new ConstantPool();
+        final ConstantNameAndType constant = new ConstantNameAndType(null,
+                new ConstantUtf8("Descriptor"));
+        final byte[] serializedConstant = new byte[] {
+                ConstantType.NAME_AND_TYPE.getTag(), 0, 0, 0, 1
+        };
+
+        assertEquals(constant.getConstantType(), ConstantType.NAME_AND_TYPE);
+        assertNull(constant.getName());
+        assertEquals(constant.getDescriptor(), new ConstantUtf8("Descriptor"));
+        assertEquals(constant.serializationSize(), 5);
+        assertArrayEquals(constant.serialize(constantPool), serializedConstant);
+    }
+
+    @Test
+    public void testConstantNullDescriptor() {
+        final ConstantPool constantPool = new ConstantPool();
+        final ConstantNameAndType constant = new ConstantNameAndType(new ConstantUtf8("Name"), null);
+        final byte[] serializedConstant = new byte[] {
+                ConstantType.NAME_AND_TYPE.getTag(), 0, 1, 0, 0
+        };
+
+        assertEquals(constant.getConstantType(), ConstantType.NAME_AND_TYPE);
+        assertEquals(constant.getName(), new ConstantUtf8("Name"));
+        assertNull(constant.getDescriptor());
+        assertEquals(constant.serializationSize(), 5);
+        assertArrayEquals(constant.serialize(constantPool), serializedConstant);
+    }
+
+    @Test
+    public void testConstantNullConstantPool() {
+        final ConstantNameAndType constant = new ConstantNameAndType(new ConstantUtf8("Name"),
+                new ConstantUtf8("Descriptor"));
+        final byte[] serializedConstant = new byte[] {
+                ConstantType.NAME_AND_TYPE.getTag(), 0, 1, 0, 2
+        };
+
+        assertEquals(constant.getConstantType(), ConstantType.NAME_AND_TYPE);
+        assertEquals(constant.getName(), new ConstantUtf8("Name"));
+        assertEquals(constant.getDescriptor(), new ConstantUtf8("Descriptor"));
+        assertEquals(constant.serializationSize(), 5);
+        assertNull(constant.serialize(null));
+    }
+}
